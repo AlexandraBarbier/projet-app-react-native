8d1731dbf210b1fb4e5e8bb57ba12365d7ce9cbe
AlexandraBarbier <graphikstaff@gmail.com>
2022-03-01 10:30:34 +0100

    Demarrage du projet + Login, Signup, Auth

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Profil from "./components/containers/Profil/Profil";
+import Auth from "./components/containers/Auth/Auth";
 
 export default function App() {
+  // let user = null;
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        <Auth />
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
